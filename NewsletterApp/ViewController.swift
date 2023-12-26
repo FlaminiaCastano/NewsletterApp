bbdf028145461881e11e082db8341912ab747004
FlaminiaCastano <flaminiacastano@gmail.com>
2023-12-26 14:47:30 -0300

    Initial Commit

---
+++ NewsletterApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsletterApp
+//
+//  Created by Flaminia Castaño on 26/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
